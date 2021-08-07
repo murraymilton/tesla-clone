f6d13fed7f532ff6d909b9624dc10543ae02facc
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-07 15:48:55 +0200

    added subcomponent section components for home, sections and header

---
+++ src/components/Section.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Section() {
+    return (
+        <Wrap>
+            Section
+        </Wrap>
+    )
+}
+
+export default Section
+
+const Wrap = styled.div `
+ width: 100vw;  // Our vertical width
+ height: 100vh; // Our vertical height
+ background: orange;
+
+`
