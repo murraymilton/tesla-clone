c487b27b6530ceb5571e0a3efd157e03522e0dc7
mmilton <84526474+murraymilton@users.noreply.github.com>
2021-08-07 16:31:14 +0200

    Created button group components

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,18 @@ import styled from 'styled-components'
 function Section() {
     return (
         <Wrap>
-            Section
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery </p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
         </Wrap>
     )
 }
@@ -14,7 +25,38 @@ export default Section
 const Wrap = styled.div `
  width: 100vw;  // Our vertical width
  height: 100vh; // Our vertical height
- background: orange;
- background-image: url('/images/solar-roof.jpg')
+ background-size: cover;
+ background-position:center;
+ background-repeat: no-repeat;
+ background-image: url('/images/model-s.jpg')
+
+`
+
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+
+`
+
+const ButtonGroup = styled.div`
+    display: flex;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+
+`
+
+const RightButton = styled(LeftButton)`  // We are inheriting the the LeftButton here to access its props
 
 `
